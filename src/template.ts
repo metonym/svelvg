7b0c785cbba466a8b7e0b0b31945393dd561f012
Eric Liu <ericyl.us@gmail.com>
2021-09-03 09:07:35 -0700

    Render-ize project, utilize vite as a testing GUI (#1)
    * chore(deps-dev): bump deps
    
    * refactor(template): type ast node interface
    
    * test: create vite app for testing methods
    
    * build: add render.yaml

--- src/template.ts
+++ src/template.ts
@@ -1,5 +1,20 @@
 import { parse, walk } from "svelte/compiler";
 
+interface Position {
+  start: number;
+  end: number;
+}
+
+interface NodeElement extends Position {
+  type: "Element";
+  name: string;
+  children: Position[];
+  attributes: {
+    name: string;
+    value: { raw: any }[];
+  }[];
+}
+
 export function templateSvelte(
   source: string,
   filename: string,
@@ -13,13 +28,13 @@ export function templateSvelte(
   classes.forEach((name) => (svg_attributes += ` class:${name}={true}`));
 
   walk(ast.html, {
-    enter(node: any) {
+    enter(node: NodeElement) {
       if (node.type === "Element" && node.name === "svg") {
-        node.children.forEach((child: any) => {
+        node.children.forEach((child) => {
           svg_children += source.slice(child.start, child.end);
         });
 
-        node.attributes.forEach(({ name, value }: any) => {
+        node.attributes.forEach(({ name, value }) => {
           if (name === "class") {
             value[0].raw.split(" ").forEach((name: string) => {
               svg_attributes += ` class:${name}={true}`;
