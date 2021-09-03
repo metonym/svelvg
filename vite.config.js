11dcc044c9622b6d7835ef858b0c42d5bdbdafe9
metonym <ericyl.us@gmail.com>
2021-09-03 11:19:40 -0700

    feat: test IS_PULL_REQUEST

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,16 @@
-export default { build: { outDir: "public" } };
+const replace = {
+  name: "replace",
+  transform(code, id) {
+    if (/index.html/.test(id)) {
+      return code.replace(
+        /IS_PULL_REQUEST/g,
+        process.env.IS_PULL_REQUEST || false
+      );
+    }
+  },
+};
+
+export default {
+  plugins: [replace],
+  build: { outDir: "public" },
+};
