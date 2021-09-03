2e15e81a396e6a68354a90b2d23a6a3b750ab81b
Eric Liu <ericyl.us@gmail.com>
2021-09-03 16:04:27 -0700

    feat: test IS_PULL_REQUEST (#2)
    * feat: test IS_PULL_REQUEST
    
    * feat: print out more RENDER_* vars
    
    * feat: link to commit in PR previews
    
    * test: refactor tests

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,28 @@
-export default { build: { outDir: "public" } };
+/**
+ * @typedef {any} DefaultValue
+ * @type {Record<string, DefaultValue>}
+ */
+const ENV_VARS = {
+  IS_PULL_REQUEST: false,
+  RENDER_GIT_BRANCH: "",
+  RENDER_GIT_COMMIT: "",
+  RENDER_GIT_REPO_SLUG: "",
+};
+
+const replace = {
+  name: "replace",
+  transform(code, id) {
+    if (/index.html/.test(id)) {
+      Object.entries(ENV_VARS).forEach(([key, value]) => {
+        code = code.replace(new RegExp(key, "g"), process.env[key] || value);
+      });
+
+      return code;
+    }
+  },
+};
+
+export default {
+  plugins: [replace],
+  build: { outDir: "public" },
+};
