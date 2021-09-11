25e390b44c9edaa773ec35a71df5007baf6b76b6
Eric Liu <ericyl.us@gmail.com>
2021-09-11 07:35:55 -0700

    fix(to-module-name): prefix module name if begins with !

--- src/to-module-name.ts
+++ src/to-module-name.ts
@@ -6,7 +6,7 @@ export function toModuleName(name: string) {
     .map((_, i) => {
       let first_char = _.slice(0, 1);
 
-      if (i === 0 && first_char.match(/[0-9]/)) {
+      if (i === 0 && first_char.match(/[0-9]|!/)) {
         first_char = "_" + first_char;
       }
 
