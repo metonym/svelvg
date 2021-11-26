8b463e1e7da53ab6030839d24f02ba6939dca839
Eric Liu <ericyl.us@gmail.com>
2021-11-26 15:36:42 -0800

    build: abridge globals

--- vite.lib.config.js
+++ vite.lib.config.js
@@ -1,16 +1,18 @@
 import pkg from "./package.json";
 
+const globals = {
+  path: "path",
+  "fs/promises": "fs",
+  "tiny-glob": "glob",
+}
+
 export default {
   build: {
     lib: { entry: "src", name: pkg.name },
     rollupOptions: {
-      external: [...Object.keys(pkg.dependencies), "fs/promises", "path"],
+      external: [...Object.keys(pkg.dependencies), ...Object.keys(globals)],
       output: {
-        globals: {
-          path: "path",
-          "fs/promises": "fs",
-          "tiny-glob": "glob",
-        },
+        globals
       },
     },
   },
