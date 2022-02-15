6054ef8bc5aee15155192b3a9bdc6400b48de541
Eric Liu <ericyl.us@gmail.com>
2022-02-15 05:51:13 -0800

    chore(template): add unit tests

---
+++ template/tests/index.test.js
@@ -0,0 +1,17 @@
+import { describe, it, expect } from "vitest";
+import * as API from "../lib";
+import pkg from "../package.json";
+
+describe("Library", () => {
+  it("has 0 dependencies", () => {
+    expect(Object.keys(pkg.dependencies ?? {}).length).toEqual(0);
+  });
+
+  it("has exports", () => {
+    expect(API.default).toBeUndefined();
+
+    const exports = Object.keys(API);
+    expect(exports.length).toEqual(401);
+    expect(exports).toMatchSnapshot();
+  });
+});
