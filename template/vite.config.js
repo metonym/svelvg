d25870ba65d2dbe5a5c40a2777115ddd47b7c2ef
Eric Liu <ericyl.us@gmail.com>
2022-02-19 12:22:38 -0800

    chore(template): set immutable to true

--- template/vite.config.js
+++ template/vite.config.js
@@ -3,8 +3,13 @@ import { defineConfig } from "vite";
 import { ghPages } from "vite-plugin-gh-pages";
 
 export default defineConfig({
-  base: "/svelvg/", // replace with "npm-name"
-  plugins: [svelte({ hot: !process.env.VITEST }), ghPages()],
+  // replace with "npm-name"
+  base: "/svelvg/",
+
+  plugins: [svelte({
+    compilerOptions: { immutable: true },
+    hot: !process.env.VITEST
+  }), ghPages()],
 
   // remove this
   server: {
