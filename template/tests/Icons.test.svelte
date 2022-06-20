2b189aea523f10b761168a888eb5ab52b45ee005
Eric Liu <ericyl.us@gmail.com>
2022-06-20 10:21:46 -0700

    chore(template): add Icons.test.svelte

---
+++ template/tests/Icons.test.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { ArrowThroughHeart, _123 as Icon_123 } from "../lib";
+  import Alt from "../lib/Alt.svelte";
+</script>
+
+<ArrowThroughHeart width={20} height="20" />
+<Alt style="fill: red" />
+<Icon_123 />
