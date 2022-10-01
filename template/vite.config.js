f123f7c7e7fe80c792d3e5d6bd08e9ae42c08fd8
Eric Liu <ericyl.us@gmail.com>
2022-10-01 11:45:58 -0700

    chore(template): replace gh-pages with GitHub workflow

--- template/vite.config.js
+++ template/vite.config.js
@@ -1,7 +1,6 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import { ghPages } from "vite-plugin-gh-pages";
 
 /** @type {import('vite').UserConfig} */
 export default {
-  plugins: [svelte(), ghPages()],
+  plugins: [svelte()],
 };
